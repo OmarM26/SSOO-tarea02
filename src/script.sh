3007b5bc389f01fe61f2af073ffff96e07e4d725
Omar Martínez Tobar <73320496+OmarM26@users.noreply.github.com>
2021-10-13 23:43:12 -0300

    Update script.sh

--- src/script.sh
+++ src/script.sh
@@ -1,2 +1,2 @@
-curl -s https://api.warframe.market/v1/items | jq 'del(.payload.items[].thumb,.payload.items[].url_name)' > Filtrados.json
+curl -s https://api.warframe.market/v1/items | jq 'del(.payload.items[].thumb,.payload.items[].url_name)' > items.json
 echo "JSON CREADO CON EXITO"
